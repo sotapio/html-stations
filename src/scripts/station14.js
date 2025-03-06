15b6630fd1bdf8dae66a97fdb20c32aed70d9818
sotapio <kitune25252@gmail.com>
2025-03-06 23:35:07 +0900

    スタイルの改善とHTML構造の更新、JavaScriptのアロー関数への変更

--- src/scripts/station14.js
+++ src/scripts/station14.js
@@ -13,6 +13,11 @@ function buildFullName(data) {
     // Station14の問題はこちらです。想定する答えをここを書いてください。
     // 氏名がわかるようにしつつ、半角スペースで繋いでください。
     // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+    const newData = {
+        ...data,
+        full_name: `${data.family_name} ${data.first_name}`
+    };
+    return newData;
 }
 
 // 出力が想定される答え
