6b6d49eb2b9c70732c6b88b12e36921d3daff51a
sotapio <kitune25252@gmail.com>
2025-04-01 22:02:29 +0900

    ユーザーリストにフルネームを追加し、データ取得関数を更新。HTML構造を整理。

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,21 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
+
+  const fullNameList = userList.map(data => {return {...data, full_name: data.family_name + " " +  data.first_name}});
+  console.log(fullNameList);
+
+  const result = await test(fullNameList)
   return await result
 }
 
-function test() {
-  return
-}
+async function test(fullNameList) {
+  const sleep = () => new Promise(resolve => {
+    setTimeout(() => {
+      resolve()
+    }, 3000)
+  })
 
+  await sleep()
+  return fullNameList;
+}
